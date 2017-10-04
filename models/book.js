53287115e0fb6d0ff879469415dde67a65175d08
thrakc3001 <cainmusicteach@gmail.com>
2017-10-03 20:54:18 -0500

    books app p1

---
+++ models/book.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var BookSchema = new Schema({
+  	title: String,
+  	author: String,
+  	image: String,
+  	release_date: String
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
