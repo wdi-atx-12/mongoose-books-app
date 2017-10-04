679f82a2be7d9205e69527496344d5427300f31f
CescoIV <crordaz@gmail.com>
2017-10-04 01:03:52 -0500

    Fran hw with challenges

--- server.js
+++ server.js
@@ -20,52 +20,13 @@ app.use(express.static('public'));
 
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-////////////////////
-//  DATA
-///////////////////
-
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-var newBookUUID = 18;
-
-
-
-
-
-
+app.use(bodyParser.json());
+//requires all exported models of the directory
+var db = require('./models');
 
 ////////////////////
 //  ROUTES
 ///////////////////
-
-
-
 
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
@@ -75,62 +36,61 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if(err){
+      console.log("index error: "+ err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  }); 
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+  var id = req.params.id;
+  db.Book.findById(id, function(err, foundBook){
+    res.json(foundBook);
+  })
 });
 
 // create new book
 app.post('/api/books', function (req, res) {
-  // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
+  var newBook = db.Book({
+    title: req.body.title,
+    author: req.body.author,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate
+  })
+
+  newBook.save(function(err,book){
+    res.send(`New book added: ${book}`);
+  })
 });
 
 // update book
 app.put('/api/books/:id', function(req,res){
-// get book id from url params (`req.params`)
-  console.log('books update', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var updateBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('updating book with index', deleteBookIndex);
-  var bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
+  var id = req.params.id;
+  var bookUpdate={
+    title: req.body.title,
+    author: req.body.author,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate
+  }
+  db.Book.findByIdAndUpdate(id, bookUpdate,{return:true}, function(err,update){
+    res.json(update);
+  })
 });
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
-  // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
-});
+  var id = req.params.id;
 
+  db.Book.findByIdAndRemove(id, function(err,deleted){
+    res.json(deleted);
+  })
+
+});
 
 
 app.listen(port, function() {
