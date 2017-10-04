159c47356abc68050dcb211de719ec576aa62631
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-10-03 22:41:49 -0500

    troubleshooting book module fail

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 // connect mongoose to your Mongo DB here
@@ -6,4 +6,6 @@ mongoose.connection.openUri(process.env.DB_CONN);
 var Books = require('books');
 
 // add exports here
-module.exports.Book = require("books.js");
+module.exports.Book = require("./book.js");
+
+module.exports = Index;
