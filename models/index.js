d5592deec6aeae9227b253c123e8f652734f4ad4
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-10-04 15:58:35 -0500

    adding Jay's edits

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 // connect mongoose to your Mongo DB here
-mongoose.connection.openUri(process.env.DB_CONN);
+mongoose.connection.openUri(`${process.env.DB_CONN}`);
 
 
 // add exports here
