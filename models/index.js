8d8001d3ab913c0528370d610c614272cad12840
miguel mejia <mejiamiguel4@gmail.com>
2017-10-03 23:03:28 -0500

    HWbooks

--- models/index.js
+++ models/index.js
@@ -1,5 +1,10 @@
 var mongoose = require("mongoose");
 
 // connect mongoose to your Mongo DB here
+DB_CONN="mongodb://mejiamiguel4:cocacola@ds161584.mlab.com:61584/wdi12mejiamiguel4"
 
 // add exports here
+
+module.exports.Book =require("./book.js");
+
+
